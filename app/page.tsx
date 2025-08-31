fc6008f1ea480676c3fbd26edf71e00837934f73
werunultras <francesco@werunultras.com>
2025-08-31 15:26:38 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -342,7 +342,6 @@ function Stat({ label, value, sub }: { label: string; value: string; sub?: strin
         {/* Spacer (1/7) */}
         <div className="md:col-span-1" />
       </div>
-    </div>
     
       {/* Import card */}
       <div className="card">
